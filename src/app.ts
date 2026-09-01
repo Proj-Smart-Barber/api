68faba2b5bbc9b4aa3a28c457be52850f9dc7d68
Alvaro Sena <me@alvarosena.com.br>
2026-08-31 23:09:02 -0300

    chore: add express js framework

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log("hello, world");
+import "dotenv/config";
+import express, { Request, Response } from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (request: Request, response: Response) => {
+  return response.json({ message: "hello, world" });
+});
+
+const port = 3333;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
