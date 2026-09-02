612f17441b7ef946530d50191f99f303742b8ce0
AlvaroSena <me@alvarosena.com.br>
2026-09-02 18:27:11 +0000

    docs: add Swagger API documentation at /docs

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import "dotenv/config";
 import express, { type Request, type Response } from "express";
+import swaggerUi from "swagger-ui-express";
 import { routes } from "./infra/http/routes";
+import { swaggerDocument } from "./infra/http/swagger";
 
 const app = express();
 app.use(express.json());
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/api", routes);
 
 app.get("/", (_request: Request, response: Response) => {
